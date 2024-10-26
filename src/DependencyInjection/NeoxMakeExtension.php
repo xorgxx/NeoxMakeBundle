15cbc0385f5d097a4a99213ff95b2592f9d27bc1
Xorg <xorg@neonatis.com>
2024-10-26 18:23:57 +0200

    bug path !!

--- src/DependencyInjection/NeoxMakeExtension.php
+++ src/DependencyInjection/NeoxMakeExtension.php
@@ -39,10 +39,10 @@
                     '%kernel.project_dir%/vendor/xorgxx/neox-make-bundle/src/templates' => 'NeoxMakeBundle',
                 ],
             ]);
-            $container->prependExtensionConfig('twig_component', [
-                'defaults' => [
-                    '%kernel.project_dir%/vendor/xorgxx/neox-make-bundle/src/Twig/Components\\' => '~',
-                ],
-            ]);
+//            $container->prependExtensionConfig('twig_component', [
+//                'defaults' => [
+//                    '%kernel.project_dir%/vendor/xorgxx/neox-make-bundle/src/Twig/Components\\' => '~',
+//                ],
+//            ]);
         }
 	}
