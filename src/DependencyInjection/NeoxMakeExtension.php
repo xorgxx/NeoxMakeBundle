99671f9cb502913d9bc808a25e2679cb144947a2
Xorg <xorg@neonatis.com>
2024-10-28 09:43:25 +0100

    bug path !!

--- src/DependencyInjection/NeoxMakeExtension.php
+++ src/DependencyInjection/NeoxMakeExtension.php
@@ -36,7 +36,7 @@
         {
             $container->prependExtensionConfig('twig', [
                 'paths' => [
-                    '%kernel.project_dir%/vendor/xorgxx/neox-make-bundle/src/templates' => 'NeoxMakeBundle',
+                    'NeoxMake\NeoxMakeBundle\templates' => 'NeoxMakeBundle',
                 ],
             ]);
             $container->prependExtensionConfig('twig_component', [
